525f11136bec76250f727b98fdd9d9364b893c99
Sila-Ozdemir <sila.ozdemir@std.bogazici.edu.tr>
2025-03-01 16:59:36 +0300

    third_trying

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 def int_to_roman(num):
-    my_dict = {1000:"M",500:"D",100:"C",50:"L",10:"X",5:"V",1:"I"}
+    my_dict = {1000:"M",900:"CM",500:"D",400:"CD",100:"C",90:"XC",50:"L",40:"XL",10:"X",9:"IX",5:"V",4:"IV",1:"I"}
     roman_string = ""
-    my_second_dict = {900:"CM",400:"CD",90:"XC",40:"XL",9:"IX",4:"IV"}
     for key in my_dict.keys():
         bolum = num // key
-        if not bolum ==4 or bolum== 9:
-            if bolum >= 1 : 
-                roman_string += bolum * my_dict[key]
-            num = num - (bolum * key)
-        else:
-            if bolum * key in my_second_dict.keys():
-                roman_string += my_second_dict[bolum *key]
-            num = num - (bolum*key)
+        if bolum >= 1 : 
+            roman_string += bolum * my_dict[key]
+        num = num - (bolum * key)
     return roman_string
                 
 
