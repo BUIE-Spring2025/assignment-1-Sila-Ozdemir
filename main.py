38ae7589c485f0019d7173c3e798ae07ee6a1f8a
Sila-Ozdemir <sila.ozdemir@std.bogazici.edu.tr>
2025-03-01 13:30:01 +0300

    first_trying

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 def int_to_roman(num):
+    my_dict = {1000:"M",500:"D",100:"C",50:"L",10:"X",5:"V",1:"I"}
+    roman_string = ""
+    for key in my_dict.keys():
+        bolum = num // key
+        if bolum >= 1 : 
+            roman_string += bolum * my_dict[key]
+        num = num - (bolum * key)
+    return roman_string
+                
+
+
+
+
+
+
     """
     Convert an integer to a Roman numeral.
 
